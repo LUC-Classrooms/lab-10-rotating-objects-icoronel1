cf20994c91398c450d635720e1d787a6e74af7c5
Isa Coronel <156938989+icoronel1@users.noreply.github.com>
2024-03-22 15:50:21 +0000

    Spin in opposite directions

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function draw() {
 
   if(mouse1 < 50) { // first, see if the mouse is within range
     if(mouseIsPressed){
-      obj1.spin()
+      obj1.spin(-0.02)
       // spin object 1 by calling its spin() method
       // you will have to create that method inside the myClass constructor ...
     } else {
@@ -42,7 +42,7 @@ function draw() {
   if(mouse2 < 50) {
     if(mouseIsPressed){
       // spin object 2 by calling its spin() method
-      obj2.spin()
+      obj2.spin(0.03)
     } else {
       obj2.reset();
     }
@@ -66,8 +66,8 @@ function myClass(tempX, tempY){
   Create a spin method here. Look at the reset() method below and the spin() method from the Sakai example for this week.
   the function assigned to this.spin should take a number argument; that number should be _added_ to the value of this.angle. The reset() method sets this.angle back to 0, and this.scale back to 1
   */
- this.spin = function(){
-  this.angle += .02;
+ this.spin = function(a){
+  this.angle += a;
   this.scale *= 0.99;
  }
   
